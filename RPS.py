127f5d15a7383a7ba25b70eea87d7643df917e7a
aaaassssuuuu007777-collab <aaaassssuuuu007777@gmail.com>
2025-10-29 18:57:43 +0530

    Improve player strategy with pattern recognition
    Enhanced player function to predict opponent's moves based on history and patterns.

--- RPS.py
+++ RPS.py
@@ -1,10 +1,33 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play:
+        opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    guess = "R"  # Default first move
+
+    if len(opponent_history) > 3:
+        # Look for patterns in the last 3 moves
+        last3 = "".join(opponent_history[-3:])
+        possible_patterns = {}
+
+        for i in range(len(opponent_history) - 3):
+            pattern = "".join(opponent_history[i:i+3])
+            next_move = opponent_history[i+3] if i+3 < len(opponent_history) else None
+            if pattern not in possible_patterns:
+                possible_patterns[pattern] = {"R": 0, "P": 0, "S": 0}
+            if next_move:
+                possible_patterns[pattern][next_move] += 1
+
+        if last3 in possible_patterns:
+            prediction = max(possible_patterns[last3], key=possible_patterns[last3].get)
+        else:
+            prediction = random.choice(["R", "P", "S"])
+    else:
+        prediction = random.choice(["R", "P", "S"])
+
+    # Choose the move that beats the predicted move
+    counter_moves = {"R": "P", "P": "S", "S": "R"}
+    guess = counter_moves[prediction]
 
     return guess
